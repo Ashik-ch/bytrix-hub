081a7568e15aa64a38d19b608eab9ebc01daee06
Ashik-ch <ashikch7117@gmail.com>
2025-10-20 04:04:31 +0400

    hub

--- src/app/components/testimonial/testimonial.component.ts
+++ src/app/components/testimonial/testimonial.component.ts
@@ -17,7 +17,7 @@ export class TestimonialComponent {
       name: 'Rasha',
       company: 'TechStart Inc.',
       role: 'CEO',
-      content: `Honestly, Bytrix completely changed our online game. In just six months, our website traffic shot up by 300%. I'm seriously impressed with how much they improved our whole digital presence.`,
+      content: `Honestly, Bytrix Hub completely changed our online game. In just six months, our website traffic shot up by 300%. I'm seriously impressed with how much they improved our whole digital presence.`,
       rating: 5,
       image: 'https://media.licdn.com/dms/image/v2/D5603AQHdGUIybC3GTA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1709126586790?e=2147483647&v=beta&t=70eN7o64dgDRvCNFS0E3px7PNUPB8xaZcAumihepGy8'
     },
@@ -25,7 +25,7 @@ export class TestimonialComponent {
       name: 'Junaid P',
       company: 'Local Restaurant',
       role: 'Owner',
-      content: `We started working with Bytrix on local SEO, and it made a huge difference. We're getting 50% more customers now.If you're a local business, you need to use them—highly recommend!`,
+      content: `We started working with Bytrix Hub on local SEO, and it made a huge difference. We're getting 50% more customers now.If you're a local business, you need to use them—highly recommend!`,
       rating: 5,
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdz5kFucdw2l7srdlElM15_DZlbH6DaE2G9Q&s'
     },
@@ -33,7 +33,7 @@ export class TestimonialComponent {
       name: 'Soumya',
       company: 'Fashion Boutique',
       role: 'Marketing Director',
-      content: `The social media campaigns Bytrix created were fantastic! They really understood our brand. Our engagement rates are through the top now—it was an amazing return on investment.`,
+      content: `The social media campaigns Bytrix Hub created were fantastic! They really understood our brand. Our engagement rates are through the top now—it was an amazing return on investment.`,
       rating: 5,
       image: 'https://media.licdn.com/dms/image/v2/C4D03AQFSNOAzmQpesA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1620468840744?e=2147483647&v=beta&t=2JeSlyoWhsUSXngmoYOjsjKAuSrGYDBUu7pIrUftfHA'
     }

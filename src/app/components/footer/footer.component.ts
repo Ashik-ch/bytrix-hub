b8f9b0ca21e9c01d3e0e24e0fe0bd8e683266aef
Ashik-ch <ashikch7117@gmail.com>
2025-10-20 17:16:40 +0400

    number changed

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -16,7 +16,7 @@ export class FooterComponent {
     { name: 'Facebook', icon: 'fab fa-facebook-f', url: 'https://www.facebook.com/bytrixhub', color: '#1877F2' },
     { name: 'Instagram', icon: 'fab fa-instagram', url: 'https://www.instagram.com/bytrixhub', color: '#E4405F' },
     { name: 'LinkedIn', icon: 'fab fa-linkedin-in', url: 'https://www.linkedin.com/company/bytrixhub', color: '#0A66C2' },
-    { name: 'WhatsApp', icon: 'fab fa-whatsapp', url: 'https://wa.me/917561084625', color: '#25D366' },
+    { name: 'WhatsApp', icon: 'fab fa-whatsapp', url: 'https://wa.me/97439956727', color: '#25D366' },
     { name: 'YouTube', icon: 'fab fa-youtube', url: 'https://www.youtube.com/@bytrixhub', color: '#FF0000' }
   ];
 
